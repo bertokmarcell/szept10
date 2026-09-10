8e780b1165bfc3c83f975fd049fe13f6486aec06
bertok.marcell <bertok.marcell@DESKTOP-7OHRVLU.akk.lan>
2026-09-10 14:42:00 +0200

    done donner

--- src/haromszog/SzemelyStatisztika.java
+++ src/haromszog/SzemelyStatisztika.java
@@ -5,7 +5,14 @@ public class SzemelyStatisztika {
     public static void main(String[] args) {
         String nev="jani";
         int szulev=2004;
+        if(!korell(eletkor(szulev))){
+            throw new IllegalArgumentException("hibas");
+        }
+        if(!nevell(nev)){
+            throw new IllegalArgumentException("hibas");
+        }
     }
+    
     static String kosz(String nev){
         return "hali"+nev;
     }
@@ -14,5 +21,14 @@ public class SzemelyStatisztika {
     }
     static int nyugi(int szam){
         return 65-szam;
+    }
+    static String bemut(int kor,String nev){
+        return "Szepnapot %s, látom maga %d -éves".formatted(nev,kor);
+    }
+    static boolean korell(int kor){
+    return kor>0&&kor<110;
+    }
+    static boolean nevell(String nevf){
+       return nevf.length()>2&& !nevf.contains("#");
     }
 }
