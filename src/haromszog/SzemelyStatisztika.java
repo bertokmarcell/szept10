450200540b473b1690ebaf16710b6d5d1d432e6e
bertok.marcell <bertok.marcell@DESKTOP-7OHRVLU.akk.lan>
2026-09-10 14:31:16 +0200

    szamely

---
+++ src/haromszog/SzemelyStatisztika.java
@@ -0,0 +1,18 @@
+
+package haromszog;
+
+public class SzemelyStatisztika {
+    public static void main(String[] args) {
+        String nev="jani";
+        int szulev=2004;
+    }
+    static String kosz(String nev){
+        return "hali"+nev;
+    }
+    static int eletkor(int ev){
+        return 2026-ev;
+    }
+    static int nyugi(int szam){
+        return 65-szam;
+    }
+}
